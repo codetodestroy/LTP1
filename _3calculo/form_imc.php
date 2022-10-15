b80795dd123c3b2cc5b05292b047b4df92b85ef5
Caio Douglas <caiodqribeiro@gmail.com>
2022-10-15 07:38:14 -0300

    Fazendo alguns ajustes

--- _3calculo/form_imc.php
+++ _3calculo/form_imc.php
@@ -6,10 +6,50 @@
     </head>
     <body>
         <h1>Cálculo de IMC</h1>
-        <form action="calcula.php" method="GET">
-            Peso:<input type="text" name="peso">
-            Altura:<input type="text" name="altura">
+        <?php 
+            if (!isset($_GET["peso"])) {
+        ?>
+        <form action="#" method="GET">
+            <label for="peso">Peso:
+                <input type="text" id="peso" name="peso">
+            </label>
+            <label for="altura">Altura:
+                <input type="text" id="altura" name="altura">
+            </label><br><br>
             <input type="submit" value="Calcular">
-        </form>
+        </form> 
+        <?php 
+        } else {
+            $peso = $_GET["peso"];
+            $altura = $_GET["altura"];
+        
+            $peso_fmt = str_replace(",", ".", $peso);
+            $altura_fmt = str_replace(",", ".", $altura);
+        
+            $imc = $peso_fmt / (pow($altura_fmt, 2));
+            $imc = number_format($imc, 2);
+        
+            echo "Seu IMC: <span style='color: green'>$imc</span><br>";
+        
+            if ($imc < 17) {
+                echo "<b>Situação:</b> Muito abaixo do peso.";
+            } elseif (($imc >= 17) && ($imc <= 18.49)) {
+                echo "<b>Situação:</b> Abaixo do peso.";
+            } elseif (($imc >= 18.50) && ($imc <= 24.99)) {
+                echo "<b>Situação:</b> Peso normal.";
+            } elseif (($imc >= 25) && ($imc <= 29.99)) {
+                echo "<b>Situação:</b> Acima do peso.";
+            } elseif (($imc >= 30) && ($imc <= 34.99)) {
+                echo "<b>Situação:</b> Obesidade I.";
+            } elseif (($imc >= 35) && ($imc <= 39.99)) {
+                echo "<b>Situação:</b> Obesidade II (Severa).";
+            } else {
+                echo "<b>Situação:</b> Obesidade III (Mórbida).";
+            }
+
+            echo "<p>Quer voltar pra página inicial? <a href=\"index.html\">Clique aqui!</a></p>";
+        } 
+        ?>  
+        
     </body>
 </html>
--- _3calculo/form_notas.php
+++ _3calculo/form_notas.php
@@ -6,15 +6,41 @@
     </head>
     <body>
         <h1>Cálculo de Média e Situação de Aluno</h1>
-        <form action="verifica_situacao.php" method="GET">
-            Aluno:<input type="text" name="aluno">
-            <br>
-            <br>
-            Nota 1:<input type="text" name="nota1">
-            Nota 2:<input type="text" name="nota2">
-            <br>
-            <br>
+        <?php
+            if (!isset($_GET["aluno"])) {
+        ?>
+        <form action="#" method="GET">
+            <label for="aluno">Nome do aluno:
+                <input type="text" id="aluno" name="aluno">
+            </label><br><br>
+            <label for="nota1">Nota 1:
+                <input type="text" id="nota1" name="nota1">
+            </label>
+            <label for="nota2">Nota 2: 
+                <input type="text" id="nota2" name="nota2">
+            </label><br><br>
             <input type="submit" value="Verificar Situação">
         </form>
+        <?php
+            } else {
+                $aluno = $_GET["aluno"];
+                $nota1 = $_GET["nota1"];
+                $nota2 = $_GET["nota2"];
+
+                $media = ($nota1 + $nota2) / 2;
+
+                echo "<b>Aluno:</b> $aluno. <br><b>Média final:</b> $media <br>";
+
+                if ($media < 4) {
+                    echo "Situação: <span style='color: red'>REPROVADO.</span>";
+                } elseif (($media >= 4) & ($media < 6)) {
+                    echo "Situação: <span style='color: orange'>EXAME FINAL.</span>";
+                } else {
+                    echo "Situação: <span style='color: green'>APROVADO</span>";
+                }
+
+                echo "<p>Quer voltar pra página inicial? <a href=\"index.html\">Clique aqui!</a></p>";
+            }
+            ?>
     </body>
 </html>
