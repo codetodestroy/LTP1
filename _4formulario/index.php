8f136b4ba41661d5b1c7e9c39fcdcfdff0d54f9b
Caio Douglas <caiodqribeiro@gmail.com>
2022-10-23 15:45:00 -0300

    json finalizado

--- _4formulario/index.php
+++ _4formulario/index.php
@@ -16,16 +16,69 @@
             $tel = $_POST['telefone'];
             $email = $_POST['endemail'];
 
+            if(strlen($nome) <= 2) {
+                $erros[] = "O nome é inválido!";
+                header("refresh: 3");
+            } 
+
+            if(!is_numeric($tel)) {
+                $erros[] = "O telefone é inválido!";
+            }
+
             if(!$email == filter_input(INPUT_POST, 'endemail', FILTER_VALIDATE_EMAIL)) {
-                $erros[] = "E-mail não está válido!";
+                $erros[] = "O e-mail é inválido!";
             }
 
             if(!empty($erros)) {
                 foreach ($erros as $erro) {
                     echo $erro;
+                    break;
                 }
             } 
 
+            $pessoa["nome"] = $nome;
+            $pessoa["sexo"] = $sexo;
+            $pessoa["dataNasc"] = $dataNasc;
+            $pessoa["tel"] = $tel;
+            $pessoa["email"] = $email;
+
+            $linha = json_encode($pessoa);
+            $arquivo = fopen('dados.txt' , "a");
+            $bits = fwrite($arquivo, $linha.PHP_EOL);
+            $arquivo = fclose($arquivo);
+
+            $file = fopen("dados.txt","r");
+
+            echo "<table id='tabela'>";
+            echo "<th>Nome</th><th>Gênero</th><th>Data de Nascimento</th><th>Telefone</th><th>E-mail</th>";
+            while(!feof($file)) 
+            {
+                $line = fgets($file);
+                
+                $pessoa = json_decode($line, true);
+            
+                if(isset($pessoa["nome"]) || isset($pessoa["sexo"]) || isset($pessoa["dataNasc"]) || isset($pessoa["tel"]) || isset($pessoa["email"])) {
+                    echo "<tr>";
+                    echo "<td>";
+                    echo $pessoa["nome"];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $pessoa["sexo"];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $pessoa["dataNasc"];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $pessoa["tel"];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $pessoa["email"];
+                    echo "</td>";
+                    echo "</tr>";
+                }
+            }
+            echo "</table>";
+            fclose($file);
         } else {
         ?>
         <h1>Formulário</h1>
@@ -34,8 +87,8 @@
                 <input type="text" name="nome" id="nome" required>
             </label><br><br>
                 Gênero:
-                <input type="radio" name="sexo" id="masc" required>Masculino
-                <input type="radio" name="sexo" id="fem">Feminino<br><br>
+                <input type="radio" name="sexo" value="Masculino" checked>Masculino
+                <input type="radio" name="sexo" value="Feminino" >Feminino<br><br>
             <label for="dataNascimento">Data de nascimento: 
                 <input type="date" name="dataNascimento" id="dataNascimento" required>
             </label><br><br>
